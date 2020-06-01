9bfdb9fa0fe4fd5cbf87f9a69b51aed826b43256
Eren <erenada@gmail.com>
2020-06-01 18:09:52 -0400

    ab

--- Scripts/Run.sh
+++ Scripts/Run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 declare -a TypesOfData=(`ls /home/erenada/BirdTree/Alignments/AllSites/Gap/m0 | cut -d "_" -f 4 | cut -d "." -f 1`)
 
 for file in {0..5}
@@ -7,3 +8,16 @@ declare -a Msp=(`ls /home/erenada/BirdTree/Alignments/$1/$2/`)
 declare -a Files=(`ls /home/erenada/BirdTree/Alignments/$1/$2/${Msp[$file]}/`)
 echo ${Files[*]} | tr " " "\n" >> FileNames.txt
 done
+
+
+for type in TypesOfData
+do
+  grep 'CDS' FileNames.txt  | grep ${Msp[$file]}
+done
+
+
+
+
+
+
+  ##output directory: /home/erenada/BirdTree/Trees/AllSites/Gap/AllSISRS/m0
