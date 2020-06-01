8147847ef73b5ec002808adce22add872347a969
Eren <erenada@gmail.com>
2020-06-01 14:45:07 -0400

    a

--- Run.sh
+++ Run.sh
@@ -1,5 +1,5 @@
 for file in {1..12}
 do
 declare -a Msp=(`ls /home/erenada/BirdTree/Alignments/$1/$2/`)
-declare -a files=(`ls /home/erenada/BirdTree/Alignments/$1/$2/${Msp[*]}`)
+echo ${Msp[*]}
 done
