422adabd9cb2a6fb4a90f4a66b6519174219d2a7
Eren <erenada@gmail.com>
2020-06-01 14:26:46 -0400

    maybe

--- Scripts/BuildTrees.sh
+++ Scripts/BuildTrees.sh
@@ -3,14 +3,15 @@
 
 #declare -a TypesOfData=(`ls /home/erenada/BirdTree/Alignments/AllSites/Gap/m0 | cut -d "_" -f 4 | cut -d "." -f 1`)
 declare -a Msp=(`ls /home/erenada/BirdTree/Alignments/$1/$2/`)
-declare -a TypesOfData=(`ls /home/erenada/BirdTree/Alignments/AllSites/Gap/$missing`)
+
+declare -a list=(`ls /home/erenada/BirdTree/Alignments/AllSites/Gap/${missing[*]}/`)
 
 
-for data in ${TypesOfData[*]}
+for data in ${list[*]}
 do
-  declare -a Files=(`ls /home/erenada/BirdTree/Alignments/$1/$2/`)
+  declare -a Files=(`ls /home/erenada/BirdTree/Alignments/$1/$2/${Msp[*]}`)
 for missing in ${Msp[*]}
 do
-echo "/home/erenada/BirdTree/Alignments/AllSites/Gap/$missing/$data}"
+echo "/home/erenada/BirdTree/Alignments/AllSites/Gap/$missing/${Files*]}"
 done
 done
