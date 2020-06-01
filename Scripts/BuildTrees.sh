7254deb72a0ff2f074c7e5b8a92cf01e7d7f1d89
Eren <erenada@gmail.com>
2020-06-01 14:19:23 -0400

    init

--- Scripts/BuildTrees.sh
+++ Scripts/BuildTrees.sh
@@ -9,6 +9,6 @@ do
   declare -a Files=(`ls /home/erenada/BirdTree/Alignments/$1/$2/`)
 for missing in ${Msp[*]}
 do
-echo "/home/erenada/BirdTree/Alignments/AllSites/Gap/$missing/$Files"
+echo "/home/erenada/BirdTree/Alignments/AllSites/Gap/$missing/${Files[@]}"
 done
 done
