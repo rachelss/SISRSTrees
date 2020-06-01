e71b9b076d9cf1de395e8328bab67c423f125be0
Eren <erenada@gmail.com>
2020-06-01 14:53:46 -0400

    a

--- Run.sh
+++ Run.sh
@@ -1,5 +1,5 @@
 for file in {1..5}
 do
 declare -a Msp=(`ls /home/erenada/BirdTree/Alignments/$1/$2/`)
-echo ${ls /home/erenada/BirdTree/Alignments/$1/$2/${Msp[$file]/*}
+echo ${ls /home/erenada/BirdTree/Alignments/$1/$2/${Msp[$file]}/*}
 done
