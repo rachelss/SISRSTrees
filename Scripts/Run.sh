5ad82aa454e3dfcf32ed98354cdd132472415672
Eren <erenada@gmail.com>
2020-06-01 18:00:30 -0400

    a

--- Scripts/Run.sh
+++ Scripts/Run.sh
@@ -5,5 +5,5 @@ for file in {0..5}
 do
 declare -a Msp=(`ls /home/erenada/BirdTree/Alignments/$1/$2/`)
 declare -a Files=(`ls /home/erenada/BirdTree/Alignments/$1/$2/${Msp[$file]}/`)
-echo ${Files[*]} >> FileNames.txt
+echo ${Files[*]} | tr " " "\n" >> FileNames.txt
 done
