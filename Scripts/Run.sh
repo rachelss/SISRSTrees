944fb4ae0e303c3ef168cad4e26be37dc539a64f
Eren <erenada@gmail.com>
2020-06-01 17:54:31 -0400

    a

--- Scripts/Run.sh
+++ Scripts/Run.sh
@@ -5,5 +5,5 @@ for file in {0..5}
 do
 declare -a Msp=(`ls /home/erenada/BirdTree/Alignments/$1/$2/`)
 declare -a Files=(`ls /home/erenada/BirdTree/Alignments/$1/$2/${Msp[$file]}/`)
-echo ${Files[*]} | tr " " "\n"
+echo ${Files[*]} | tr " " "\n" | cut -d "." -f 1
 done
