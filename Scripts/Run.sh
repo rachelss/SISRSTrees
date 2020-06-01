fa3f813acf1bc3bd5e53195f9ba2595e056f531e
Eren <erenada@gmail.com>
2020-06-01 15:21:22 -0400

    a

--- Scripts/Run.sh
+++ Scripts/Run.sh
@@ -4,6 +4,6 @@ declare -a TypesOfData=(`ls /home/erenada/BirdTree/Alignments/AllSites/Gap/m0 | 
 for file in {0..5}
 do
 declare -a Msp=(`ls /home/erenada/BirdTree/Alignments/$1/$2/`)
-declare Files=(`$(ls /home/erenada/BirdTree/Alignments/$1/$2/${Msp[$file]}/)`)
+declare -a Files=(`$(ls /home/erenada/BirdTree/Alignments/$1/$2/${Msp[$file]}/)`)
 echo ${Files[*]}
 done
