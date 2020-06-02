84fcfc37863233b3034a47cd4e22be588bb032f5
Eren <erenada@gmail.com>
2020-06-02 10:41:53 -0400

    abc

--- Scripts/Run.sh
+++ Scripts/Run.sh
@@ -11,11 +11,16 @@ done
 
 declare -a TypesOfData=(`ls /home/erenada/BirdTree/Alignments/AllSites/Gap/m0 | cut -d "_" -f 4 | cut -d "." -f 1`)
 
-
-for type in ${TypesOfData[@]}
+input="/home/erenada/BirdTree/Scripts/FileNames.txt"
+while IFS= read- r line
 do
-paste /home/erenada/BirdTree/Scripts/IQCommand.txt /home/erenada/BirdTree/Scripts/FileNames.txt
-done
+  paste /home/erenada/BirdTree/Scripts/IQCommand.txt /home/erenada/BirdTree/Scripts/FileNames.txt
+done <alltogether.txt
+
+
+# for type in ${TypesOfData[@]}
+# do
+# done
 
 # for type in "${TypesOfData[@]}"
 # do
