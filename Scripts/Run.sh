37f7f765f0521682fb560c6c02c4e405e7a1bf5c
Eren <erenada@gmail.com>
2020-06-02 10:42:54 -0400

    abc

--- Scripts/Run.sh
+++ Scripts/Run.sh
@@ -12,7 +12,7 @@ done
 declare -a TypesOfData=(`ls /home/erenada/BirdTree/Alignments/AllSites/Gap/m0 | cut -d "_" -f 4 | cut -d "." -f 1`)
 
 input="/home/erenada/BirdTree/Scripts/FileNames.txt"
-while IFS= read- r line
+while IFS= read -r line
 do
   paste /home/erenada/BirdTree/Scripts/IQCommand.txt /home/erenada/BirdTree/Scripts/FileNames.txt
 done <FileNames.txt
