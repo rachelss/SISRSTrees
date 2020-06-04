498e315a3a8aaccdf01b08a0e0776fc97742742b
Eren <erenada@gmail.com>
2020-06-03 21:18:24 -0400

    init

---
+++ Scripts/NoGap/NoGapRun.sh
@@ -0,0 +1,10 @@
+declare -a TypesOfData=(`ls /home/erenada/BirdTree/Alignments/AllSites/Gap/m0 | cut -d "_" -f 4 | cut -d "." -f 1`)
+
+for file in {0..5}
+do
+declare -a Msp=(`ls /home/erenada/BirdTree/Alignments/$1/$2/`)
+declare -a Files=(`ls /home/erenada/BirdTree/Alignments/$1/$2/${Msp[$file]}/`)
+echo ${Files[*]} |  tr " " "\n" >> FileNames.txt
+done
+
+declare -a TypesOfData=(`ls /home/erenada/BirdTree/Alignments/AllSites/Gap/m0 | cut -d "_" -f 4 | cut -d "." -f 1`)
